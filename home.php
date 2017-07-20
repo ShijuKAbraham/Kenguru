2f65708c76e22009545ed5eddc353fd7db5bfb21
vyshnavgireesh <vyshnavxlr8@gmail.com>
2017-07-21 05:07:51 +0530

    initial

---
+++ home.php
@@ -0,0 +1,36 @@
+<html>
+    <title>
+        Kenguru-Home
+    </title>
+    <head>
+        <meta name="Kenguru" http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <link rel="stylesheet" type="text/css" href="style1.css">
+        <link rel="stylesheet" type="text/css" href="search.css">
+    </head>
+   
+    <body>  
+        <div id="headcontainer">
+            <p><a href="#"><img src="images/twitter.png" height="30px" width="30px"></a>
+            <a href="#"><img src="images/facebook.png" height="30px" width="30px"></a>
+            <tab1></tab1>
+            <font size="4.5em"><strong>SignUp/Login</strong></font></p>
+        </div>
+        <div id="toolcontainer">
+            <tab4></tab4>
+            <img src="images/logo2.png"width="160px"height="100px">
+            <tab2></tab2>Home<tab3></tab3>How It Works
+           <div id="wrap">
+              <form action="google.com" autocomplete="on">
+              <input id="search" name="search" type="text" placeholder="What're we looking for ?"><input id="search_submit" value="Rechercher" type="submit">
+              </form>
+            </div>
+        </div>
+        <div id="triangle1">
+        
+        </div>
+        <div id="triangle2">
+        
+        </div>
+    </body>
+    
+</html>
